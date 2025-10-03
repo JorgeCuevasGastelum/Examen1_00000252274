28668d185d66bd12b8479acd1320e9f765f76908
JorgeCuevasGastelum <jorge.cuevas252274@potros.itson.edu.mx>
2025-10-03 02:30:23 -0700

    Diagrama de Clases y Secuencia de Analisis
    - Se agrego al archivo de StarUML el diagrama de clases de analisis y el diagrama de secuencia
    - Se avanzo un poco a los frames principales del caso de uso

--- Examen1_00000252274_InscribirClases/src/main/java/main.java
+++ Examen1_00000252274_InscribirClases/src/main/java/main.java
@@ -1,3 +1,7 @@
+
+import Presentacion.JPantallaInscripcion;
+import Presentacion.JPantallaMenu;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +13,8 @@
  */
 public class main {
      public static void main(String[] args) {
-         System.out.println("a");
+         JPantallaMenu menu = new JPantallaMenu();
+         menu.setVisible(true);
+         
      }
 }
